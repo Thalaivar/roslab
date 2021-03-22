8852e2a2c396c30b9cb070c9d7160333f9d66a5d
Dhruv Laad <dhruvlaad@gmail.com>
2021-03-22 11:27:56 +0530

    explore

--- explore.py
+++ explore.py
@@ -6,10 +6,7 @@ import rospy
 
 from std_msgs.msg import Float64
 
-def get_joint_angle(x, n):
-	return math.pi * math.sin(x / n)
-
-def rrbot_exploratory_publisher(n=100):
+def rrbot_exploratory_publisher(update_rate=100, rps=2 * math.pi / 60):
 	rospy.init_node("joint_positions_node", anonymous=True)
 	
 	joint_names = ["joint0", "joint1", "joint2"]
@@ -17,7 +14,8 @@ def rrbot_exploratory_publisher(n=100):
 						f"/rrbot/{x}_position_controller/command", 
 						Float64, 
 						queue_size=10) for x in joint_names}
-	rate = rospy.Rate(100)
+	rate = rospy.Rate(update_rate)
+	rps = rps / update_rate
 
 	"""
 		The strategy followed to explore and find the object is:
@@ -30,24 +28,23 @@ def rrbot_exploratory_publisher(n=100):
 		this strategy
 	"""
 	curr_ang = {name: 0 for name in joint_names}
-	curr_ang["joint1"] = 0
-	curr_ang["joint2"] = -math.pi * n / 8
+	curr_ang["joint1"] = math.pi / 4
 	while not rospy.is_shutdown():
 		# rotate base joint for 2*pi radians
-		# curr_ang["joint0"] %= (2 * math.pi * n)	
-		pubs["joint0"].publish(get_joint_angle(curr_ang["joint0"], n))
+		curr_ang["joint0"] %= 2 * math.pi	
+		pubs["joint0"].publish(curr_ang["joint0"])
 
 		# fix first joint at specified angle
-		pubs["joint1"].publish(get_joint_angle(curr_ang["joint1"], n))
+		pubs["joint1"].publish(curr_ang["joint1"])
 
-		# rotate first joint between -pi / 2 and pi / 2
-		# curr_ang["joint2"] = -math.pi * n / 6
-		# while curr_ang["joint2"] <= math.pi * n / 6:
-		# 	pubs["joint2"].publish(get_joint_angle(curr_ang["joint2"], n))
-		# 	curr_ang["joint2"] += 1
-		# 	rate.sleep()
-		pubs["joint2"].publish(get_joint_angle(curr_ang["joint2"], n))
-		# curr_ang["joint0"] += 1
+		# rotate first joint between pi / 4 and 3 * pi / 4
+		curr_ang["joint2"] = math.pi / 4
+		while curr_ang["joint2"] <= math.pi * n / 6:
+			pubs["joint2"].publish(curr_ang["joint2"])
+			curr_ang["joint2"] += rps
+			rate.sleep()
+		
+		curr_ang["joint0"] += rps
 		rate.sleep()
 
 if __name__ == "__main__":
