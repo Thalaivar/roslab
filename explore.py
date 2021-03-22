88f76b751a86f9aab8e42338d17130273eb1e67b
Thalaivar <dhruvlaad@gmail.com>
2021-03-22 15:41:55 +0530

    assignment done

--- explore.py
+++ explore.py
@@ -5,68 +5,92 @@ import yaml
 import rospy
 
 from std_msgs.msg import Float64
+from sensor_msgs.msg import Image
+from image_processing import image_processing
 
-def hold_at_angle(joint_angles=[math.pi / 2, math.pi / 4, 0]):
-	rospy.init_node("joint_positions_node", anonymous=True)
-	
-	joint_names = [f"joint{i}" for i in range(len(joint_angles))]
-	pubs = {x: rospy.Publisher(
+from cv_bridge import CvBridge
+
+class Explore:
+	def __init__(self, n_joints=3, update_freq=100, rps=2 * math.pi / 60):
+		rospy.init_node("joint_positions_node", anonymous=True)
+
+		self.joint_names = [f"joint{x}" for x in range(n_joints)]
+		self.pubs = {x: rospy.Publisher(
 						f"/rrbot/{x}_position_controller/command", 
 						Float64, 
-						queue_size=10) for x in joint_names}
-	rate = rospy.Rate(100)
-
-	curr_ang = {name: x for name, x in zip(joint_names, joint_angles)}
-	while not rospy.is_shutdown():
-		for name, pub in pubs.items():
-			pub.publish(curr_ang[name])
-
-		rate.sleep()
-
-def exploratory(update_rate=100, rps=2 * math.pi / 60):
-	rospy.init_node("joint_positions_node", anonymous=True)
-	
-	joint_names = ["joint0", "joint1", "joint2"]
-	pubs = {x: rospy.Publisher(
-						f"/rrbot/{x}_position_controller/command", 
-						Float64, 
-						queue_size=10) for x in joint_names}
-	rate = rospy.Rate(update_rate)
-	rps = rps / update_rate
-
-	"""
-		The strategy followed to explore and find the object is:
-			1. Rotate base joint from 0 -> 2*pi
-			2. Fix joint1's angle at pi/4
-			3. For every base joint, rotate joint2 from -pi/4 -> pi/2
+						queue_size=10) for x in self.joint_names}
 		
-		The assumption is made that the object is on the ground. If this
-		assumption holds, then we should be able to find the object with
-		this strategy
-	"""
-	curr_ang = {name: 0 for name in joint_names}
-	curr_ang["joint1"] = math.pi / 4
-	while not rospy.is_shutdown():
-		# rotate base joint for 2*pi radians
-		curr_ang["joint0"] %= 2 * math.pi	
-		pubs["joint0"].publish(curr_ang["joint0"])
-
-		# fix first joint at specified angle
-		pubs["joint1"].publish(curr_ang["joint1"])
-
-		# rotate first joint between -pi / 4 and pi / 2
-		curr_ang["joint2"] = -math.pi / 4
-		while curr_ang["joint2"] <= math.pi / 4:
-			pubs["joint2"].publish(curr_ang["joint2"])
-			curr_ang["joint2"] += rps
-			rate.sleep()
+		self.sub = rospy.Subscriber("/rrbot/camera1/image_raw", Image, self.process_raw_image)
 		
-		curr_ang["joint0"] += (math.pi / 4)
-		rate.sleep()
+		self.mask_pub = rospy.Publisher("/rrbot/bbox_image", Image, queue_size=10)
+
+		self.rate = rospy.Rate(update_freq)
+		self.rps = rps / update_freq
+
+		self.bridge = CvBridge()
+		self.detected = False
+		self.detect_thresh = 60000
+
+	def process_raw_image(self, raw_image: Image):
+		image = self.bridge.imgmsg_to_cv2(raw_image, desired_encoding="passthrough")
+		area, _, masked_image = image_processing(image)
+
+		if area > 0:
+			self.mask_pub.publish(self.bridge.cv2_to_imgmsg(masked_image, encoding="passthrough"))
+		else:
+			self.mask_pub.publish(self.bridge.cv2_to_imgmsg(image, encoding="passthrough"))
+		if area > self.detect_thresh:
+			self.detected = True
+		
+	def hold_at_angle(self, joint_angles=[math.pi / 2, math.pi / 4, 0]):
+		curr_ang = {name: x for name, x in zip(self.joint_names, joint_angles)}
+		while not rospy.is_shutdown():
+			for name, pub in self.pubs.items():
+				pub.publish(curr_ang[name])
+
+			self.rate.sleep()
+
+	def exploratory(self):
+		"""
+			The strategy followed to explore and find the object is:
+				1. Rotate base joint from 0 -> 2*pi
+				2. Fix joint1's angle at pi/4
+				3. For every base joint, rotate joint2 from -pi/4 -> pi/2
+			
+			The assumption is made that the object is on the ground. If this
+			assumption holds, then we should be able to find the object with
+			this strategy
+		"""
+		curr_ang = {name: 0 for name in self.joint_names}
+		curr_ang["joint1"] = math.pi / 4
+		while not rospy.is_shutdown() and not self.detected:
+			# rotate base joint for 2*pi radians
+			curr_ang["joint0"] %= 2 * math.pi	
+			self.pubs["joint0"].publish(curr_ang["joint0"])
+
+			# fix first joint at specified angle
+			self.pubs["joint1"].publish(curr_ang["joint1"])
+
+			# rotate first joint between 0 and pi / 2
+			curr_ang["joint2"] = 0
+			while curr_ang["joint2"] <= math.pi / 4 and not self.detected:
+				self.pubs["joint2"].publish(curr_ang["joint2"])
+
+				curr_ang["joint2"] += self.rps
+				self.rate.sleep()
+			
+			if not self.detected:
+				curr_ang["joint0"] += (math.pi / 4)
+			self.rate.sleep()
+
+		if self.detected:
+			hold_angles = [x for _, x in curr_ang.items()]
+			rospy.loginfo(f"Object detected! Holding at: {[round(x * 180 / math.pi, 2) for x in hold_angles]}")
+			self.hold_at_angle(hold_angles)
 
 if __name__ == "__main__":
 	try:
-		exploratory(update_rate=100)
-		# hold_at_angle()
+		exp = Explore()
+		exp.exploratory()
 	except rospy.ROSInterruptException: 
 		pass
--- image_processing.py
+++ image_processing.py
@@ -1,15 +1,13 @@
 import cv2
 import imutils
 import numpy as np
-
-def read_image_from_camera():
-	
+from copy import deepcopy
 
 def image_processing(image):
 	# convert black squares to white
 	THRESH = 40
 	idx = np.where((image <= [THRESH, THRESH, THRESH]).all(axis=2))
-	thresh_image = image
+	thresh_image = deepcopy(image)
 	thresh_image[idx] = [255, 255, 255]
 	
 	# threshold image to make bgd black
@@ -20,14 +18,16 @@ def image_processing(image):
 	cont, _ = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	areas = [cv2.contourArea(c) for c in cont]
 
-	max_area, max_cont = max(areas), cont[np.argmax(areas)]
+	if len(areas) > 0:
+		max_area, max_cont = max(areas), cont[np.argmax(areas)]
+		# create bounding box for largest contour
+		x, y, w, h = cv2.boundingRect(max_cont)
+		image = cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
+	else:
+		max_area, max_cont = 0, 0
 
-	# # create bounding box for largest contour
-	# x, y, w, h = cv2.boundingRect(max_cont)
-	# cv2.imshow("Result", cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2))
-	# cv2.waitKey(0)
 
-	print(max_area)
+	return max_area, max_cont, image
 
 if __name__ == '__main__':
 	image = cv2.imread("/Users/dhruvlaad/IIT/ID6100/roslab/ref.jpg")
